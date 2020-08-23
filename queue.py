bd76bebd12733722ba0edc5bf9d08c151e207e24
DarthHamza <hamzamakia@gmail.com>
2020-08-23 10:20:45 +0300

    completed queue task

--- queue.py
+++ queue.py
@@ -1,10 +1,10 @@
 class Node:
-    def __init__(self, data, next=None):
-        self.data = data
+    def __init__(self, group_size, next=None):
+        self.group_size = group_size
         self.next = next
 
-    def get_data(self):
-        return self.data
+    def get_group_size(self):
+        return self.group_size
 
     def get_next(self):
         return self.next
@@ -19,9 +19,10 @@ class Queue:
         self.back = back
         self.limit = limit
         self.length = 0
+        self.waiting_time = 0
 
     def peek(self):
-        return self.front
+        return self.waiting_time
 
     def is_full(self):
         return self.length == self.limit
@@ -29,17 +30,35 @@ class Queue:
     def is_empty(self):
         return self.length == 0
 
+    def add_node(self, data):
+        new_node = Node(data)
+        if self.is_empty():
+            self.front = new_node
+        else:
+            self.back.set_next(new_node)
+        self.back = new_node
+        self.length += 1
+        self.waiting_time += (data*0.5)
+
     def enqueue(self, data):
         if self.is_full():
             print("No more space!")
         else:
-            new_node = Node(data)
-            if self.is_empty():
-                self.front = new_node
-            else:
-                self.back.set_next(new_node)
-            self.back = new_node
-            self.length += 1
+            groups, remainder = divmod(data, 12)
+            for i in range(groups):
+                self.add_node(12)
+            if remainder:
+                self.add_node(remainder)
+
+    def enqueue(self, data):
+        if self.is_full():
+            print("No more space!")
+        else:
+            people_num = data
+            while people_num > 12:
+                self.add_node(12)
+                people_num -= 12
+            self.add_node(people_num)
 
     def dequeue(self):
         if self.is_empty():
@@ -48,36 +67,22 @@ class Queue:
             removed_node = self.front
             self.front = removed_node.get_next()
             self.length -= 1
-            return removed_node.get_data()
+            self.waiting_time -= (removed_node.get_group_size() * 0.5)
+            return removed_node.get_group_size()
 
-print("You're all here to bend the knee, so get in line...")
-# Creating a Queue instance
-throne_room = Queue()
-
-throne_room.enqueue("Jon Snow")
-throne_room.enqueue("Arya Stark")
-throne_room.enqueue("Tyrion Lannister")
-throne_room.enqueue("Daenerys Targaryen")
-throne_room.enqueue("Sansa Stark")
-throne_room.enqueue("Ygritte")
-throne_room.enqueue("Brienne of Tarth")
-throne_room.enqueue("Tormund GiantsBane")
-throne_room.enqueue("Oberyn Martel")
-throne_room.enqueue("Margaery Tyrell")
-
-# Queue Overflow
-throne_room.enqueue("Theon Greyjoy")
-
-print(throne_room.dequeue())
-print(throne_room.dequeue())
-print(throne_room.dequeue())
-print(throne_room.dequeue())
-print(throne_room.dequeue())
-print(throne_room.dequeue())
-print(throne_room.dequeue())
-print(throne_room.dequeue())
-print(throne_room.dequeue())
-print(throne_room.dequeue())
-
-# Queue Underflow
-print(throne_room.dequeue())
+hamzas_vc = Queue(10)
+print(hamzas_vc.waiting_time)
+print("-"*50)
+hamzas_vc.enqueue(5)
+hamzas_vc.enqueue(15)
+hamzas_vc.enqueue(25)
+hamzas_vc.enqueue(1)
+print(hamzas_vc.length) # 7
+print("-"*50)
+print(hamzas_vc.waiting_time) # 23
+print("-"*50)
+hamzas_vc.dequeue()
+print(hamzas_vc.length) # 6
+print("-"*50)
+print(hamzas_vc.waiting_time) # 20.5
+print("-"*50)
