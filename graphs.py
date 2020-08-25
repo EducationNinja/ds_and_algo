2194ac6cf53f2e435ca9de08332e65b34763ed74
DarthHamza <hamzamakia@gmail.com>
2020-08-25 09:35:47 +0300

    completed graphs solutions

--- graphs.py
+++ graphs.py
@@ -17,10 +17,10 @@ class Graph:
     def add_vertex(self, vertix):
         self.vertices[vertix.data] = vertix
 
-    def add_edge(self, vertix_a, vertix_b, weight):
-        self.vertices[vertix_a.data].add_edge(vertix_b.data, weight)
+    def add_edge(self, vertix_a, vertix_b, price, time):
+        self.vertices[vertix_a.data].add_edge(vertix_b.data, {"price": price, "time": time})
         if not self.directed:
-            self.vertices[vertix_b.data].add_edge(vertix_a.data, weight)
+            self.vertices[vertix_b.data].add_edge(vertix_a.data, {"price": price, "time": time})
 
     def path_exists(self, vertix_a, vertix_b):
         to_visit = [vertix_a]
@@ -37,21 +37,33 @@ class Graph:
         return False
 
 
-kuwait = Graph(directed=False)
+travel_destinations = Graph(directed=False)
+cities = [
+    Vertex("Kuwait"), Vertex("Dubai"), Vertex("Colombo"), Vertex("Male"),
+    Vertex("Doha"), Vertex("Tokyo"), Vertex("Oslo")
+]
 
-kuwait_city = Vertex("Kuwait City")
-salmiya = Vertex("Salmiya")
-shaab = Vertex("Shaab")
-shuwaikh = Vertex("Shuwaikh")
+for city in cities:
+    travel_destinations.add_vertex(city)
 
-kuwait.add_vertex(kuwait_city)
-kuwait.add_vertex(salmiya)
-kuwait.add_vertex(shaab)
-kuwait.add_vertex(shuwaikh)
+travel_destinations.add_edge(cities[0], cities[1], 120, 2)
+travel_destinations.add_edge(cities[0], cities[2], 200, 4)
+travel_destinations.add_edge(cities[2], cities[2], 60, 1)
+travel_destinations.add_edge(cities[1], cities[4], 100, 1.5)
+travel_destinations.add_edge(cities[4], cities[5], 500, 11)
+travel_destinations.add_edge(cities[1], cities[6], 300, 6)
 
-kuwait.add_edge(kuwait_city, shaab, 10)
-kuwait.add_edge(kuwait_city, shuwaikh, 15)
-kuwait.add_edge(shaab, salmiya, 20)
+print("Where are you travelling from?")
+for city in travel_destinations.vertices.keys():
+    print(f"- {city}")
 
-print(kuwait.path_exists('Kuwait City', 'Salmiya'))
-print(kuwait.path_exists('Shaab', 'Shuwaikh'))
+from_city = input("From: ")
+
+print(f"These are all the direct flights from {from_city}")
+for option in travel_destinations.vertices[from_city].get_edges():
+    print(f"- {option}")
+
+to_city = input("To: ")
+
+weight = travel_destinations.vertices[from_city].edges[to_city]
+print(f"This is gonna cost you: {weight['price']} dollars and you'll be on the flight for {weight['time']} hours.")
